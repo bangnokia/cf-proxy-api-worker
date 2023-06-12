41b08d6b35c93c7d6f7ed515f447a628f8add1f5
bangnokia <bangnokia@gmail.com>
2023-06-12 17:35:41 +0700

    remove comments

--- src/index.ts
+++ src/index.ts
@@ -12,21 +12,20 @@ export default {
       return new Response("FBI, put your hands up 🦀!", { "status": 401 })
     }
 
-    // target url is the https:/ + the path of the request and the query string
     const targetUrl = `https:/${url.pathname}${url.search}`
 
     const requestOptions = {
       method: request.method,
       headers: {
         ...request.headers,
-        'Origin': targetUrl, // Set the target URL as the origin header
+        'Origin': targetUrl,
       },
     }
 
     const response = await fetch(targetUrl, requestOptions)
 
     const corsHeaders = {
-      'Access-Control-Allow-Origin': '*', // Allow all origins (you can customize this if needed)
+      'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type',
     }
